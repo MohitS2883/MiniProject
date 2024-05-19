39e92696c96bc7dc8f315372842a54af68b94f7a
shirtpanttie <107927186+shirtpanttie@users.noreply.github.com>
2024-05-20 00:28:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template, request
+from flask import *
 import os
 from detect import pred
 from suggestions import returnSuggestions
+import threading
+from langchainlogic import main as langchain_main
 app = Flask(__name__)
 
 @app.route('/')
-def trial():
+def home():
+    return render_template('home.html')
+
+@app.route('/input')
+def getinput():
     return render_template('input.html')
 
 @app.route('/trial', methods=['GET', 'POST'])  
-def home():
+def result():
     if request.method == 'POST':
         print("Form submitted successfully!")
         image = request.files['inpimage']
@@ -26,7 +32,21 @@ def home():
         return render_template('index.html', ans=ans, path=image_path,sugsend = sugg)
     return render_template('input.html')
 
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+
+@app.route('/home')
+def redirect_home():
+    return redirect("/")
+
+def run_chatlit_server():
+    # Run the LangChain logic
+    langchain_main()
+
 
 if __name__ == "__main__":
+    langchain_thread = threading.Thread(target=run_chatlit_server)
+    langchain_thread.start()
     app.run(host="0.0.0.0", debug=True)
  
