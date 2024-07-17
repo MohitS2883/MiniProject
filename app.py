3af1739b633482ad25eef189349d191b465fb615
shirtpanttie <107927186+shirtpanttie@users.noreply.github.com>
2024-07-17 13:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import *
 import os
 from detect import pred
 from suggestions import returnSuggestions
-import threading
-from langchainlogic import main as langchain_main
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,21 +12,16 @@ def home():
 def getinput():
     return render_template('input.html')
 
-@app.route('/trial', methods=['GET', 'POST'])  
+@app.route('/trial', methods=['POST'])  
 def result():
     if request.method == 'POST':
-        print("Form submitted successfully!")
         image = request.files['inpimage']
         path = "static/Image"
-        print(image)
         os.makedirs(path, exist_ok=True)  
         image_path = os.path.join(path, image.filename)
         image.save(image_path)
-        print("Image saved successfully at:", image_path)
         ans = pred(image_path)
         sugg = returnSuggestions(ans)
-        print("Prediction result:", ans)
-        print("suggestions",sugg)
         return render_template('index.html', ans=ans, path=image_path,sugsend = sugg)
     return render_template('input.html')
 
@@ -40,13 +33,7 @@ def chatbot():
 def redirect_home():
     return redirect("/")
 
-def run_chatlit_server():
-    # Run the LangChain logic
-    langchain_main()
-
 
 if __name__ == "__main__":
-    langchain_thread = threading.Thread(target=run_chatlit_server)
-    langchain_thread.start()
     app.run(host="0.0.0.0", debug=True)
  
